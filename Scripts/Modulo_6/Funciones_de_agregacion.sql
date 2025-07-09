f00cec116bb55a9bfedeece4a61d26e11263a944
RafaGo89 <135170128+RafaGo89@users.noreply.github.com>
2025-07-09 12:51:02 -0600

    Add files via upload

--- Scripts/Modulo_6/Funciones_de_agregacion.sql
+++ Scripts/Modulo_6/Funciones_de_agregacion.sql
@@ -14,17 +14,115 @@ USE registro_climatico;
 */
 
 -- Función COUNT()
--- Contar la cantidad de ciudades
+-- Contar la cantidad de filas de la tabla ciudad (Incluye valores NULL)
 SELECT COUNT(*)
 FROM ciudad;
 
--- Podemos usar también la id como columna a contar
+-- Podemos usar también la id como columna a contar (NO incluye valores NULL)
 SELECT COUNT(id) AS 'Cantidad de ciudades'
 FROM ciudad;
 
--- Usamos COUNT(DISTINCT) para obtener el conteo de valores ÚNICOS
+-- Usamos COUNT(DISTINCT) para obtener el conteo de valores ÚNICOS (NO incluye valores NULL)
 SELECT nombre, id_pais
 FROM ciudad;
 
 SELECT COUNT(DISTINCT id_pais)
 FROM ciudad;
+
+-- Obtenemos el promedio de altitud de las estaciones con AVG()
+SELECT AVG(altitud) AS altitud_promedio
+FROM estacion;
+
+-- Obtenemos la altitud máxima con MAX()
+SELECT MAX(altitud) AS maxima_Altitud
+FROM estacion;
+
+-- Obtenemos la altitud mínima con MIN()
+SELECT MIN(altitud) AS minima_Altitud
+FROM estacion;
+
+-- Obtenemos la suma total de las poblaciones de las ciudades
+SELECT SUM(poblacion) AS suma_poblacion
+FROM ciudad;
+
+-- Obtenemos la desviación estándar de las altitudes de las estaciones
+SELECT STD(altitud) AS desviacion_estandar_altitud
+FROM estacion;
+
+-- Obtenemos la varianza de las altitudes de las estaciones
+SELECT VARIANCE(altitud) AS desviacion_estandar_altitud
+FROM estacion;
+
+-- Podemos poner más 1 función de agregado en una sentencia
+-- Obtenemos la suma total de las poblaciones de las ciudades
+SELECT SUM(poblacion) AS Suma, AVG(poblacion) AS Promedio
+FROM ciudad;
+
+-- No podemos hacer esto: traer una columna y luego una función de agregación
+SELECT nombre, AVG(poblacion)
+FROM ciudad;
+
+-- Podemos usar WHERE en conjunto con las funciones agregadas
+
+-- Obtener la suma de las poblaciones de las siguientes ciudades: Tonala, Barcelona y París
+SELECT SUM(poblacion)
+FROM ciudad
+WHERE nombre IN ('Tonala', 'Barcelona', 'París');
+
+-- Otra manera de hacerlo
+SELECT SUM(poblacion)
+FROM ciudad
+WHERE nombre = 'Tonala' OR nombre = 'Barcelona' OR nombre = 'París';
+
+-- Podemos usar variables en conjunto con las funciones de agregación
+
+-- Declaramos una variable
+SET @max_poblacion = 0;
+
+-- Guardamos un valor en esa variable con la sentencia SELECT INTO
+
+-- Guardamos el valor de la población más grande en nuestra variable
+SELECT MAX(poblacion) INTO @max_poblacion 
+FROM ciudad;
+
+-- Mostramos el valor de la variable
+SELECT @max_poblacion;
+
+-- Obtenemos el nombre y poblacion de la ciudad con mayor poblacion
+SELECT nombre AS 'Ciudad más poblada', poblacion
+FROM ciudad
+WHERE poblacion = @max_poblacion;
+
+-- Obtenemos la ciudad 'Más' pequeña en orden alfabético de A-Z
+SELECT MIN(nombre)
+FROM ciudad;
+
+-- Obtenemos la ciudad 'Más' grande en orden alfabético de Z-A
+SELECT MAX(nombre)
+FROM ciudad;
+
+-- Pequeños ejercicios
+
+-- Obtener nombre y población de las ciudades con una población
+-- menor a la del promedio de todas las ciudades. Ordenarlas descendentemente
+SET @promedio_poblacion = 0;
+
+SELECT AVG(poblacion) INTO @promedio_poblacion
+FROM ciudad;
+
+-- Visualizamos el promedio de poblacion
+SELECT @promedio_poblacion;
+
+SELECT nombre, poblacion
+FROM ciudad
+WHERE poblacion < @promedio_poblacion
+ORDER BY poblacion DESC;
+
+-- Obtener la población de la ciudad más pequeña de Argentina
+SELECT MIN(poblacion)
+FROM ciudad
+WHERE id_pais = 'ARG';
+
+-- Seleccionar la fecha de instalación más reciente de las estaciones
+SELECT MAX(fecha_instalacion)
+FROM estacion;
