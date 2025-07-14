611b91f38810f4840f0a8698b26be807e21d0759
RafaGo89 <rafael.rodriguez7962@alumnos.udg.mx>
2025-07-14 16:01:08 -0600

    Avances

---
+++ Scripts/Modulo_6/HAVING.sql
@@ -0,0 +1,15 @@
+/*
+	Introducción a las bases de datos SQL
+	Modulo 6: Cláusula HAVING
+*/
+
+USE registro_climatico;
+
+/* Sintaxis básica para usar el operador BETWEEN
+
+	SELECT column_name
+	FROM table_name
+	GROUP BY column_name
+	HAVING condition;
+    
+*/
